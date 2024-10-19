ad50c79defc37738348ec083195a69148fecc7fb
billygeen123 <63556269+billygeen123@users.noreply.github.com>
2024-10-19 12:45:29 +0300

    sasaa

--- Database/ApplicationDbContext.cs
+++ Database/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using JoskiTGBot2024.Models;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace JoskiTGBot2024.Database
 {
@@ -7,6 +9,28 @@ namespace JoskiTGBot2024.Database
     {
         public DbSet<User> Users { get; set; }
 
+        public ApplicationDbContext()
+        {
+            try
+            {
+                var dbCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if (dbCreator is not null)
+                {
+                    if (!dbCreator.CanConnect())
+                    {
+                        dbCreator.Create();
+                    }
+                    if (!dbCreator.HasTables())
+                    {
+                        dbCreator.CreateTables();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=bot.db");
